77a470abe4c8aece872d54ad8b3bac10ccf34ef0
Martynenko.O.S <martynenko.oleksii1122@vu.cdu.edu.ua>
2022-12-06 18:54:02 +0200

    LB

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace It_s_easy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
+        }
+    }
+}
